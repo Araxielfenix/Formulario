6c926bba605879e3f0a761c71b8cf68a15988d5c
Araxiel <araxielfenix@outlook.com>
2022-02-26 17:16:55 -0600

    https://leafletjs.com/reference.html#draggable

--- Script.js
+++ Script.js
@@ -19,9 +19,24 @@ function showPosition(position) {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYXJheGllbGZlbml4IiwiYSI6ImNqeHBudXRqdDBqcTAzY3F1dGNmZGcxd2UifQ.xh-aFGjDG3PMZU2WfdGaQA'
 }).addTo(map);
+//Enables dragging marker
+var contador = 0;
+var puntoOrigen;
+var puntoDestino;
+map.on('click', function(e) {
+   //Si el contador es 0, se crea un marcador con el nombre origen.
+    if (contador == 0) {
+        var marker = L.marker([e.latlng.lat, e.latlng.lng], {draggable: true}).addTo(map);
+        puntoOrigen = marker.getLocation;
+        marker.bindPopup("<b>Origen</b>").openPopup();
+        contador++;
+    }
+    //Si el contador es 1, se crea un marcador con el nombre destino.
+    else if (contador == 1) {
+        var marker = L.marker([e.latlng.lat, e.latlng.lng], {draggable: true}).addTo(map);
+        puntoDestino = marker.getLocation;
+        marker.bindPopup("<b>Destino</b>").openPopup();
+        contador++;
+    }
+});
 }
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
-
-map.on('click', onMapClick);
