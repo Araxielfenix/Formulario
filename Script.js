6a1d4b886f65518955c6ef642bcfb75e655b6169
Araxielfenix <araxielfenix@outlook.com>
2022-02-28 09:00:08 -0600

    Obtener dirección y trazar ruta.

--- Script.js
+++ Script.js
@@ -30,6 +30,7 @@ map.on('click', function(e) {
         puntoOrigen = marker.getLocation;
         marker.bindPopup("<b>Origen</b>").openPopup();
         contador++;
+        getDireccion(e.latlng.lat, e.latlng.lng);
     }
     //Si el contador es 1, se crea un marcador con el nombre destino.
     else if (contador == 1) {
@@ -37,6 +38,37 @@ map.on('click', function(e) {
         puntoDestino = marker.getLocation;
         marker.bindPopup("<b>Destino</b>").openPopup();
         contador++;
+        getDireccion(e.latlng.lat, e.latlng.lng);
     }
 });
+trazarRuta();
+}
+//Traza la ruta entre los puntos origen y destino.
+function trazarRuta() {
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + puntoOrigen + ';' + puntoDestino + '?geometries=geojson&access_token=pk.eyJ1IjoiYXJheGllbGZlbml4IiwiYSI6ImNqeHBudXRqdDBqcTAzY3F1dGNmZGcxd2UifQ.xh-aFGjDG3PMZU2WfdGaQA';
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var coords = data.routes[0].geometry.coordinates;
+            var line = L.polyline(coords).addTo(map);
+            map.fitBounds(line.getBounds());
+        }
+    });
+}
+// Funcion para obtener la dirección de una latitud y longitud.
+function getDireccion(lat, lng) {
+    var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + lat + ',' + lng + '.json?access_token=pk.eyJ1IjoiYXJheGllbGZlbml4IiwiYSI6ImNqeHBudXRqdDBqcTAzY3F1dGNmZGcxd2UifQ.xh-aFGjDG3PMZU2WfdGaQA';
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var address = data.features[0].place_name;
+            var direccion = address.split(',');
+            var direccionFinal = direccion[0];
+            document.getElementById('direccion').innerHTML = direccionFinal;
+        }
+    });
 }
